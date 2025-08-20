e05db158f3e86125956627bc879c982ac47223b2
Dennis Döring <big_atze@hotmail.com>
2025-08-20 12:29:08 +0200

    + Refactored Calculations.cs to be more clear and more maintainable.

--- SGCLibrary/Calculations.cs
+++ SGCLibrary/Calculations.cs
@@ -3,104 +3,79 @@
 public static class Calculations
 {
     public const double GAS_CONSTANT = 8.31446261815324;
+    public const double ABSOLUTE_ZERO_CELSIUS = -273.15;
 
+    private static void ValidatePositive(string paramName, double value)
+    {
+        if (value <= 0.0)
+            throw new ArgumentOutOfRangeException(paramName, $"The value of {paramName} cannot be <= 0.0!");
+    }
+
+    private static void ValidateAboveAbsoluteZero(string paramName, double value)
+    {
+        if (value <= ABSOLUTE_ZERO_CELSIUS)
+            throw new ArgumentOutOfRangeException(paramName, $"The value of {paramName} cannot be <= {ABSOLUTE_ZERO_CELSIUS} (absolute zero)!");
+    }
+
+    /// <summary>
+    /// Calculates the number of moles (n) using the ideal gas law.
+    /// </summary>
     public static double CalculateMoles(double P, double V, double T)
     {
-        if (P <= 0.0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(P), $"The value of {nameof(P)} can not be <= 0.0!");
-        }
-
-        if (V <= 0.0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(V), $"The value of {nameof(V)} can not be <= 0.0!");
-        }
-
-        if (T <= 0.0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(T), $"The value of {nameof(T)} can not be <= 0.0!");
-        }
-
+        ValidatePositive(nameof(P), P);
+        ValidatePositive(nameof(V), V);
+        ValidatePositive(nameof(T), T);
         return (P * V) / (GAS_CONSTANT * T);
     }
 
+    /// <summary>
+    /// Calculates the volume (V) using the ideal gas law.
+    /// </summary>
     public static double CalculateVolume(double n, double T, double P)
     {
-        if (n <= 0.0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(n), $"The nalue of {nameof(n)} can not be <= 0.0!");
-        }
-
-        if (T <= 0.0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(T), $"The nalue of {nameof(T)} can not be <= 0.0!");
-        }
-
-        if (P <= 0.0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(P), $"The nalue of {nameof(P)} can not be <= 0.0!");
-        }
-
+        ValidatePositive(nameof(n), n);
+        ValidatePositive(nameof(T), T);
+        ValidatePositive(nameof(P), P);
         return (n * GAS_CONSTANT * T) / P;
     }
 
+    /// <summary>
+    /// Calculates the pressure (P) using the ideal gas law.
+    /// </summary>
     public static double CalculatePressure(double n, double T, double V)
     {
-        if (n <= 0.0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(n), $"The nalue of {nameof(n)} can not be <= 0.0!");
-        }
-
-        if (T <= 0.0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(T), $"The nalue of {nameof(T)} can not be <= 0.0!");
-        }
-
-        if (V <= 0.0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(V), $"The value of {nameof(V)} can not be <= 0.0!");
-        }
-
+        ValidatePositive(nameof(n), n);
+        ValidatePositive(nameof(T), T);
+        ValidatePositive(nameof(V), V);
         return (n * GAS_CONSTANT * T) / V;
     }
 
+    /// <summary>
+    /// Calculates the temperature (T) using the ideal gas law.
+    /// </summary>
     public static double CalculateTemperature(double P, double V, double n)
     {
-        if (P <= 0.0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(P), $"The nalue of {nameof(P)} can not be <= 0.0!");
-        }
-
-        if (V <= 0.0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(V), $"The value of {nameof(V)} can not be <= 0.0!");
-        }
-
-        if (n <= 0.0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(n), $"The nalue of {nameof(n)} can not be <= 0.0!");
-        }
-
+        ValidatePositive(nameof(P), P);
+        ValidatePositive(nameof(V), V);
+        ValidatePositive(nameof(n), n);
         return (P * V) / (GAS_CONSTANT * n);
     }
 
+    /// <summary>
+    /// Converts Celsius to Kelvin.
+    /// </summary>
     public static double ConvertToKelvin(double tempInCelsius)
     {
-        if (tempInCelsius <= -273.15)
-        {
-            throw new ArgumentOutOfRangeException(nameof(tempInCelsius), $"The nalue of {nameof(tempInCelsius)} can not be <= -273.15 (absolute zero)!");
-        }
-
+        ValidateAboveAbsoluteZero(nameof(tempInCelsius), tempInCelsius);
         return tempInCelsius + 273.15;
     }
 
+    /// <summary>
+    /// Converts Kelvin to Celsius.
+    /// </summary>
     public static double ConvertToCelsius(double tempInKelvin)
     {
-        if (tempInKelvin <= 0.0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(tempInKelvin), $"The nalue of {nameof(tempInKelvin)} can not be <= 0.0 (absolute zero)!");
-        }
-
+        ValidatePositive(nameof(tempInKelvin), tempInKelvin);
         return tempInKelvin - 273.15;
     }
 }
