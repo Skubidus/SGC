b02b0be7352210e14c7e59261bc34f0875c25f2e
Dennis Döring <big_atze@hotmail.com>
2025-08-21 15:55:10 +0200

    + Added functionality so that when you switch tabs, the old values from the previous tab will be taken over to the left side values of the new tab. This way you get a seemless experience and can easily swap between what you are trying to calculate.

--- SGCWpfApp/MainWindow.xaml.cs
+++ SGCWpfApp/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using SGCWpfApp.ViewModels;
+using SGCWpfApp.Views;
 
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace SGCWpfApp
 {
@@ -22,6 +25,228 @@ namespace SGCWpfApp
 
             InitializeComponent();
             DataContext = new MainWindowViewModel();
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var control = sender as TabControl;
+            if (control is null)
+            {
+                return;
+            }
+
+            var newTab = control.SelectedContent as UserControl;
+            if (newTab is null)
+            {
+                return;
+            }
+
+            var removedItems = e.RemovedItems.Cast<TabItem>();
+            UserControl? oldTab = null;
+
+            if (removedItems.Any())
+            {
+                oldTab = removedItems.First().Content as UserControl;
+                if (oldTab is null)
+                {
+                    return;
+                }
+
+                Debug.WriteLine($"Old Tab: {oldTab.Name}");
+            }
+
+            Debug.WriteLine($"New Tab: {newTab.Name}");
+            Debug.WriteLine("");
+
+            // sorry! i know this sucks but it works ¯\_(ツ)_/¯
+            if (newTab.Name == nameof(MolesView))
+            {
+                TransferPropertiesToMolesTab(newTab, oldTab);
+            }
+            else if (newTab.Name == nameof(PressureView))
+            {
+                TransferPropertiesToPressureTab(newTab, oldTab);
+            }
+            else if (newTab.Name == nameof(VolumeView))
+            {
+                TransferPropertiesToVolumeTab(newTab, oldTab);
+            }
+            else if (newTab.Name == nameof(TemperatureView))
+            {
+                TransferPropertiesToTemperatureTab(newTab, oldTab);
+            }
+        }
+
+        private static void TransferPropertiesToMolesTab(UserControl newTab, UserControl? oldTab)
+        {
+            if (oldTab is null)
+            {
+                return;
+            }
+
+            var newTabViewModel = ((MolesViewViewModel)newTab.DataContext);
+
+            var oldTabViewModelType = oldTab.DataContext.GetType();
+            object dataContext = oldTab.DataContext;
+
+            var propertyInfo = oldTabViewModelType.GetProperty(nameof(newTabViewModel.Pressure));
+            if (propertyInfo is not null)
+            {
+                var value = propertyInfo.GetValue(dataContext);
+                if (value is not null)
+                {
+                    newTabViewModel.Pressure = (double)value;
+                }
+            }
+
+            propertyInfo = oldTabViewModelType.GetProperty(nameof(newTabViewModel.Volume));
+            if (propertyInfo is not null)
+            {
+                var value = propertyInfo.GetValue(dataContext);
+                if (value is not null)
+                {
+                    newTabViewModel.Volume = (double)value;
+                }
+            }
+
+            propertyInfo = oldTabViewModelType.GetProperty(nameof(newTabViewModel.Temperature));
+            if (propertyInfo is not null)
+            {
+                var value = propertyInfo.GetValue(dataContext);
+                if (value is not null)
+                {
+                    newTabViewModel.Temperature = (double)value;
+                }
+            }
+        }
+
+        private static void TransferPropertiesToPressureTab(UserControl newTab, UserControl? oldTab)
+        {
+            if (oldTab is null)
+            {
+                return;
+            }
+
+            var newTabViewModel = ((PressureViewViewModel)newTab.DataContext);
+
+            var oldTabViewModelType = oldTab.DataContext.GetType();
+            object dataContext = oldTab.DataContext;
+
+            var propertyInfo = oldTabViewModelType.GetProperty(nameof(newTabViewModel.Moles));
+            if (propertyInfo is not null)
+            {
+                var value = propertyInfo.GetValue(dataContext);
+                if (value is not null)
+                {
+                    newTabViewModel.Moles = (double)value;
+                }
+            }
+
+            propertyInfo = oldTabViewModelType.GetProperty(nameof(newTabViewModel.Temperature));
+            if (propertyInfo is not null)
+            {
+                var value = propertyInfo.GetValue(dataContext);
+                if (value is not null)
+                {
+                    newTabViewModel.Temperature = (double)value;
+                }
+            }
+
+            propertyInfo = oldTabViewModelType.GetProperty(nameof(newTabViewModel.Volume));
+            if (propertyInfo is not null)
+            {
+                var value = propertyInfo.GetValue(dataContext);
+                if (value is not null)
+                {
+                    newTabViewModel.Volume = (double)value;
+                }
+            }
+        }
+
+        private static void TransferPropertiesToVolumeTab(UserControl newTab, UserControl? oldTab)
+        {
+            if (oldTab is null)
+            {
+                return;
+            }
+
+            var newTabViewModel = ((VolumeViewViewModel)newTab.DataContext);
+
+            var oldTabViewModelType = oldTab.DataContext.GetType();
+            object dataContext = oldTab.DataContext;
+
+            var propertyInfo = oldTabViewModelType.GetProperty(nameof(newTabViewModel.Moles));
+            if (propertyInfo is not null)
+            {
+                var value = propertyInfo.GetValue(dataContext);
+                if (value is not null)
+                {
+                    newTabViewModel.Moles = (double)value;
+                }
+            }
+
+            propertyInfo = oldTabViewModelType.GetProperty(nameof(newTabViewModel.Temperature));
+            if (propertyInfo is not null)
+            {
+                var value = propertyInfo.GetValue(dataContext);
+                if (value is not null)
+                {
+                    newTabViewModel.Temperature = (double)value;
+                }
+            }
+
+            propertyInfo = oldTabViewModelType.GetProperty(nameof(newTabViewModel.Pressure));
+            if (propertyInfo is not null)
+            {
+                var value = propertyInfo.GetValue(dataContext);
+                if (value is not null)
+                {
+                    newTabViewModel.Pressure = (double)value;
+                }
+            }
+        }
+
+        private static void TransferPropertiesToTemperatureTab(UserControl newTab, UserControl? oldTab)
+        {
+            if (oldTab is null)
+            {
+                return;
+            }
+
+            var newTabViewModel = ((TemperatureViewViewModel)newTab.DataContext);
+
+            var oldTabViewModelType = oldTab.DataContext.GetType();
+            object dataContext = oldTab.DataContext;
+
+            var propertyInfo = oldTabViewModelType.GetProperty(nameof(newTabViewModel.Pressure));
+            if (propertyInfo is not null)
+            {
+                var value = propertyInfo.GetValue(dataContext);
+                if (value is not null)
+                {
+                    newTabViewModel.Pressure = (double)value;
+                }
+            }
+
+            propertyInfo = oldTabViewModelType.GetProperty(nameof(newTabViewModel.Volume));
+            if (propertyInfo is not null)
+            {
+                var value = propertyInfo.GetValue(dataContext);
+                if (value is not null)
+                {
+                    newTabViewModel.Volume = (double)value;
+                }
+            }
+
+            propertyInfo = oldTabViewModelType.GetProperty(nameof(newTabViewModel.Moles));
+            if (propertyInfo is not null)
+            {
+                var value = propertyInfo.GetValue(dataContext);
+                if (value is not null)
+                {
+                    newTabViewModel.Moles = (double)value;
+                }
+            }
         }
     }
 }
--- SGCWpfApp/ViewModels/MainWindowViewModel.cs
+++ SGCWpfApp/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace SGCWpfApp.ViewModels;
 public partial class MainWindowViewModel : ObservableObject
